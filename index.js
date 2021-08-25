b43f05ff4b6b2b58a445546a78e4581392ff2249
github username <github email address>
2021-08-25 15:36:32 -0400

    commit tree 20dfd791674447570230be9dfe8bd7e474ab63f4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+function map (array, func){
+  let arr= []
+  for (let i= 0; i< array.length; i++){
+      arr.push(func(array[i]))
+  }
+    return arr
+}
+
+
+
+
+function reduce(array, func, startingPoint){
+    let total //accumulator
+  if (!startingPoint){
+    total = array[0];
+    for (let i =1; i < array.length; i++){
+    total = func(array[i], total);
+      } 
+    } else {
+        total = startingPoint
+     for (let i =0; i < array.length; i++){
+         total = func(array[i], total);
+   }
+  }
+return total;
+
+}
